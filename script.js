543d478ca5fba54a85e0d9a37622322fba7605e9
Esther White <esteewhite@hotmail.com>
2022-12-07 19:20:09 +0200

    missing parts

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-
 const main = document.querySelector("main");
+const voicesSelect = document.getElementById("voices");
+const textarea = document.getElementById("text");
+const readBTN = document.getElementById("read");
+const toggleBTN = document.getElementById("toggle");
+const closeBTN = document.getElementById("close");
 
 const dataArr = [
-    {
-        image: 'assets/images/angular.jpg',
-        text: "stuff",
-        image: 'assets/images/angular.jpg',
-        text: "stuff",
-    }
-]
+  {
+    image: "assets/images/angular.jpg",
+    text: "Angular Essential Training",
+    link: "https://google.com"
+  },
+];
 
 dataArr.forEach(createBox);
 
-// Create certificate boxes
+// Create speech boxes
 function createBox(item) {
-    const box = document.createElement("div");
-  
-    const { image, text } = item;
-    // instead of using item.image & item.text
-  
-    box.classList.add("box");
-    box.innerHTML = `
-      <img src="${image}" alt="${text}"/>
-      <p class="info">${text}</p> 
-    `;
+  const box = document.createElement("div");
+
+  const { image, text, link } = item;
+  // instead of using item.image & item.text
+
+  box.classList.add("box");
+  box.innerHTML = `
+  <a href="${link}" target="_blank"><img src="${image}" alt="${text}"/></a>
+    <p class="info">${text}</p>  
+  `;
+  main.appendChild(box);
 }
